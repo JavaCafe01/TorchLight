8733a26c9ae697a21ee1ec872c5b67db591effc4
JavaCafe01 <JavaCafe01@users.noreply.github.com>
2018-03-26 22:07:11 -0700

    Initial commit

---
+++ app/src/main/java/com/gsnathan/torchlight/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gsnathan.torchlight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
